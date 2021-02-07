78a33d9ce3844d1c0154033ca0766f24b26a3400
AlphaYoung111 <729346015@qq.com>
2021-02-07 15:40:05 +0800

    2-7

--- service/config/plugin.js
+++ service/config/plugin.js
@@ -1,9 +1,22 @@
+/*
+ * @Description:
+ * @Author: yjr
+ * @Date: 2021-02-07 14:45:18
+ * @LastEditTime: 2021-02-07 15:37:10
+ * @LastEditors: yjr
+ */
 'use strict';
+/* eslint-disable */
 
 /** @type Egg.EggPlugin */
-module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
-};
+// module.exports = {
+//   // had enabled by egg
+//   // static: {
+//   //   enable: true,
+//   // }
+// };
+
+exports.mysql = {
+  enable: true,
+  package: 'egg-mysql',
+}
